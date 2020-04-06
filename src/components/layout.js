781b4a361b0790854be5f185829274413f1d6f49
acculief <yajousaki@gmail.com>
2020-04-07 00:59:32 +0900

    add layout style

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,65 +4,40 @@ import { Link } from "gatsby"
 import { rhythm, scale } from "../utils/typography"
 
 const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  let header
+  const ListLink = props => (
+    <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+      <Link to={props.to}>{props.children}</Link>
+    </li>
+  )
 
-  if (location.pathname === rootPath) {
-    header = (
-      <h1
-        style={{
-          ...scale(1.5),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <h3
-        style={{
-          fontFamily: `Montserrat, sans-serif`,
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h3>
-    )
-  }
   return (
     <div
       style={{
+        backgroundColor: "#f5f5f5",
+        padding: `3rem 1.5rem`,
+        maxWidth: "50rem",
         marginLeft: `auto`,
         marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `3rem 1.5rem`,
+        minHeight: "100rem",
+        borderStyle: "solid",
+        borderColor: "#dbdbdb",
+        borderRightWidth: 1,
+        borderLeftWidth: 1,
+        borderTopWidth: 0,
+        borderBottomWidth: 0,
       }}
     >
-      <header>{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
+      <header style={{ marginBottom: `1.5rem` }}>
+        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+          <h3 style={{ display: `inline` }}>{title}</h3>
+        </Link>
+        <ul style={{ listStyle: `none`, float: `right` }}>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/about/">About</ListLink>
+          <ListLink to="/contact/">Contact</ListLink>
+        </ul>
+      </header>
+      {children}
     </div>
   )
 }
